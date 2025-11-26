f45e04ad8af15bb313dc8028bf2f6e68177e130b
skicodes <skalrashed02@hotmail.com>
2025-11-26 17:44:52 +0000

    5

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Battery Street!"
+# Version switch: set this to True for Holberton, False for Battery Street
+holberton_version = True
+
+if holberton_version:
+    str1 = "Welcome to "
+    str2 = "Holberton School!"
+else:
+    str1 = "Welcome to "
+    str2 = "Battery Street!"
+
 print(str1 + str2)
-# Done
