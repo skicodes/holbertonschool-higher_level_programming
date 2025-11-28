b97ead9dbba477ab8632f9aacf143c42a0a1a1fc
skicodes <skalrashed02@hotmail.com>
2025-11-28 19:56:13 +0000

    9

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """
+    Prints a string in uppercase followed by a new line.
+    """
+    for c in str:
+        if 'a' <= c <= 'z':
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
