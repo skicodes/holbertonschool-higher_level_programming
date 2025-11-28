e4f50fc017ffd12ea67ee26b4c9d389c4e3f3c46
skicodes <skalrashed02@hotmail.com>
2025-11-28 20:49:18 +0000

    1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Import a function and use it to add two numbers"""
 
-if __name__ == "__main__":
-    add_0 = __import__('add_0').add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add_0(a, b)))
+from add_0 import add
+
+a = 1
+b = 2
+
+print("{} + {} = {}".format(a, b, add(a, b)))
