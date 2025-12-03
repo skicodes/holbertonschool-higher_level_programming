2305465cb236992adf9957c7d814c2611069af26
skicodes <skalrashed02@hotmail.com>
2025-12-03 21:42:33 +0000

    new

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Function that replaces all occurrences of an element by another
+in a new list.
+"""
+
+
 def search_replace(my_list, search, replace):
-    """Returns a new list where all occurrences of `search` are replaced by `replace`."""
+    """
+    Returns a new list with all occurrences of `search` replaced by `replace`.
+    """
     return [replace if x == search else x for x in my_list]
