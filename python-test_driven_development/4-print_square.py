cc52cf62180f7159e8ac79075eb120260ca5c660
skicodes <skalrashed02@hotmail.com>
2025-12-06 10:54:03 +0000

    4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module provides a function that prints a square using '#'.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with '#' character.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
