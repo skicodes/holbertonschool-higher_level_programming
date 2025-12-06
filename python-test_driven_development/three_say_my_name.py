b6dce1f3aaf043ac972172eecc8f98725d9d2db9
skicodes <skalrashed02@hotmail.com>
2025-12-06 00:03:58 +0000

    2

--- python-test_driven_development/three_say_my_name.py
+++ python-test_driven_development/three_say_my_name.py
@@ -5,6 +5,7 @@ Module three_say_my_name
 Contains a function that prints a person's full name.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints 'My name is <first name> <last name>'.
@@ -22,4 +23,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print(f"My name is {first_name} {last_name}".rstrip())
-
