478cf5b1e957d408df7d3dbe9ed65d5a63de6169
skicodes <skalrashed02@hotmail.com>
2025-12-06 11:05:46 +0000

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x integers of a list.
-
-    Args:
-        my_list (list): List containing any type of elements.
-        x (int): Number of elements to access in my_list.
-
-    Returns:
-        int: The number of integers successfully printed.
-    """
     count = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            except (ValueError, TypeError):
-                continue
-    except IndexError:
-        pass
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            raise
     print()
     return count
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides element by element two lists up to list_length.
-    
-    Args:
-        my_list_1 (list): First list
-        my_list_2 (list): Second list
-        list_length (int): Number of elements to divide
-    
-    Returns:
-        list: List of division results
-    """
-    result = []
+    new_list = []
     for i in range(list_length):
-        div = 0
         try:
-            div = my_list_1[i] / my_list_2[i]
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            div = 0
-        except TypeError:
+            result = 0
+        except (TypeError, ValueError):
             print("wrong type")
-            div = 0
+            result = 0
         except IndexError:
             print("out of range")
-            div = 0
+            result = 0
         finally:
-            result.append(div)
-    return result
+            new_list.append(result)
+    return new_list
